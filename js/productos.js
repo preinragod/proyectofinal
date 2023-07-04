10ca10a9114acee2186bbc71f9cbb0b6bc8483de
Cristian Olguin <cristian.n.olguin@gmail.com>
2023-07-04 16:01:27 -0300

    2023-07-04 - MODIFICACION RUTA

--- js/productos.js
+++ js/productos.js
@@ -5,7 +5,7 @@ return {
 productos:[],
 //url:'http://localhost:5000/productos',
 // si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
-url:'http://cristiannolguin.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
+url:'../productos', // si ya lo subieron a pythonanywhere
 error:false,
 cargando:true,
 /*atributos para el guardar los valores del formulario */
